e6762e5369482d1902c425c3e174b76d866fa5da
chandhana9 <44041293+chandhana9@users.noreply.github.com>
2019-01-09 15:25:46 -0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 mvn clean
 mvn package
-mvn exec:java -Dexec.mainClass="bot.discord.dbsk.App" -s "/opt/maven/conf/settings.xml"
+mvn exec:java -Dexec.mainClass="bot.discord.dbsk.App" -s "/usr/share/maven/conf/settings.xml"
